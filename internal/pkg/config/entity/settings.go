648865ddeebfb7562177c166ca42de5787c669cc
konstantin.litvin <konstantin.litvin@enkod.io>
2024-07-09 17:27:47 +0300

    fix config value for client

--- internal/pkg/config/entity/settings.go
+++ internal/pkg/config/entity/settings.go
@@ -1,10 +1,6 @@
 package entity
 
-import (
-	cKafka "github.com/confluentinc/confluent-kafka-go/v2/kafka"
-)
-
 type Settings struct {
-	KafkaProducer cKafka.ConfigMap `json:"kafkaProducer"`
-	KafkaConsumer cKafka.ConfigMap `json:"kafkaConsumer"`
+	KafkaProducer map[string]interface{} `json:"kafkaProducer"`
+	KafkaConsumer map[string]interface{} `json:"kafkaConsumer"`
 }
